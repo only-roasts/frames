2f6ad6d1a6809037326a6f0b3f7e61b9f2cb0d7d
krishnan74 <divyakrishnan.r74@gmail.com>
2025-02-03 11:56:10 +0530

    feat- initial frame done using fetching metadata from pinata

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -1,76 +1,81 @@
 /** @jsxImportSource frog/jsx */
 
-import { Button, Frog, TextInput } from 'frog'
-import { devtools } from 'frog/dev'
+import axios from "axios";
+import { Button, Frog, TextInput } from "frog";
+import { devtools } from "frog/dev";
 // import { neynar } from 'frog/hubs'
-import { handle } from 'frog/next'
-import { serveStatic } from 'frog/serve-static'
+import { handle } from "frog/next";
+import { serveStatic } from "frog/serve-static";
 
 const app = new Frog({
-  assetsPath: '/',
-  basePath: '/api',
+  assetsPath: "/",
+  basePath: "/api",
   // Supply a Hub to enable frame verification.
   // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
-  title: 'Frog Frame',
-})
+  title: "Frog Frame",
+});
 
 // Uncomment to use Edge Runtime
 // export const runtime = 'edge'
 
-app.frame('/', (c) => {
-  const { buttonValue, inputText, status } = c
-  const fruit = inputText || buttonValue
+app.frame("/:tokenId", async (c) => {
+  const roastData = [
+    {
+      roast: "You’ve spent more on gas fees than on your coffee this month! ☕",
+      walletAddress: "0x1234567890",
+      flameCount: 3,
+    },
+  ];
+  const { buttonValue, inputText, status } = c;
+  const fruit = inputText || buttonValue;
+
+  console.log(c.req.param("tokenId"));
+  const tokenId = c.req.param("tokenId");
+
+  const ipfsMetaDataResponse = await axios.get(
+    `https://white-official-scallop-559.mypinata.cloud/ipfs/${tokenId}`
+  );
+
+  console.log(ipfsMetaDataResponse.data);
+  // const roastResponse = await axios.get(
+  //   "http://localhost:3001/api/generate-roast"
+  // );
+
+  // const roastImageResponse = await axios.post(
+  //   "http://localhost:3001/api/generate-image",
+  //   {
+  //     roast: roastData[0].roast,
+  //     walletAddress: roastData[0].walletAddress,
+  //     flameCount: roastData[0].flameCount,
+  //   }
+  // );
+
+  // const { image } = roastImageResponse.data;
+
   return c.res({
+    // image ? (`data:image/png;base64,${image}`) :
     image: (
-      <div
-        style={{
-          alignItems: 'center',
-          background:
-            status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
-              : 'black',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
-        }}
-      >
-        <div
-          style={{
-            color: 'white',
-            fontSize: 60,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
-        </div>
+      <div tw="flex bg-white text-black">
+        <p>hi {ipfsMetaDataResponse.data.name}</p>
       </div>
     ),
+
     intents: [
-      <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="apples">Apples</Button>,
-      <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+      <Button action="/getRoast" value="goldfinger">
+        Get Your Roast
+      </Button>,
+      <Button.Mint target="eip155:84532:0x07E909d6B1857E7a90a6a05DacE89A1d8E234Bfc:1">
+        Mint Now
+      </Button.Mint>,
+      <Button.Link href="https://google.com">Vist Website</Button.Link>,
     ],
-  })
-})
+  });
+});
 
-devtools(app, { serveStatic })
+devtools(app, { serveStatic });
 
-export const GET = handle(app)
-export const POST = handle(app)
+export const GET = handle(app);
+export const POST = handle(app);
 
 // NOTE: That if you are using the devtools and enable Edge Runtime, you will need to copy the devtools
 // static assets to the public folder. You can do this by adding a script to your package.json:
